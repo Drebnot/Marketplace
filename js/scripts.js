81a714d2863a327e63fbb9eef5814fc38d853159
Cory West-Forbes and Jesse Drebin <cory@>
2018-03-21 16:25:30 -0700

    add modal feature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,33 @@
 $(function(){
-  $('form#text-form').submit(function(event){
+    // Get the modal
+  $('form#checkout').submit(function(event) {
+    var name = $('#name').val();
+    var address = $('#address').val();
+
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+    }
+
+    $('.add-name').text(name);
+    $('.add-address').text(address);
+
     event.preventDefault();
-    var name = $('text-input').val();
-    var food = $("input:radio[name=food]:checked").val();
-    var music = $("#music").val();
-    var favoriteColor = $("#color").val();
-    $('body').css('background-color', favoriteColor);
   });
 });
