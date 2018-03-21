aa9fa615c5f32591bbe18e2b4bd5d09128e09f9a
Cory West-Forbes and Jesse Drebin <cory@>
2018-03-21 16:30:16 -0700

    add jquery to modal.

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,26 @@ $(function(){
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
+    // btn.onclick = function() {
+    //   modal.style.display = "block";
+    // }
+    //
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       modal.style.display = "none";
     }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-    }
+    //
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //   if (event.target == modal) {
+    //       modal.style.display = "none";
+    //   }
+    // }
 
     $('.add-name').text(name);
     $('.add-address').text(address);
+
+    $('.modal').show();
 
     event.preventDefault();
   });
