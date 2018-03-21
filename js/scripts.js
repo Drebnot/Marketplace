89c44272f7cb4919793ef0f8e79f6f5529173424
Cory West-Forbes and Jesse Drebin <cory@>
2018-03-21 15:32:56 -0700

    add favorite color background feature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(function(){
   $('form#text-form').submit(function(event){
+    event.preventDefault();
     var name = $('text-input').val();
     var food = $("input:radio[name=food]:checked").val();
     var music = $("#music").val();
     var favoriteColor = $("#color").val();
+    $('body').css('background-color', favoriteColor);
   });
 });
