d9faf95970200be201052f18cdc8d983568d6801
Cory West-Forbes and Jesse Drebin <jesse@>
2018-03-21 15:14:23 -0700

    add new form inputs: radio buttonsand select class

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(function(){
   $('form#text-form').submit(function(event){
     var name = $('text-input').val();
-    
+    var food = $("input:radio[name=food]:checked").val();
+    var music = $("#music").val();
+    var favoriteColor = $("#color").val();
   });
-
 });
